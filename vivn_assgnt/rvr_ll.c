029985ea8377b576878932e0a62bb5544274d6f9
rsvn <rizwanshaz2004@gmail.com>
2026-01-06 23:05:15 +0530

    rv_lnk_lst

--- vivn_assgnt/rvr_ll.c
+++ vivn_assgnt/rvr_ll.c
@@ -6,51 +6,89 @@ int data;
 struct node* nxt;
 } node;
 
-void add_node(node** head,node* nd){
+void append_node(node* head,int data){
+  node* tmp=head;
+  node* last_node;
+  while(tmp){
+  last_node=tmp;
+  tmp=tmp->nxt;
+  }
 
-  nd->nxt=*head;
-  *head=nd;
 
-}
-
-
-void print_ll(node* head) {
-    node* tmp = head;
-    while (tmp) {
-        printf("%d\n", tmp->data);
-        tmp = tmp->nxt;
-    }
-}
-
+  node* new_node=malloc(sizeof(node));
+  new_node->data=data;
+  new_node->nxt=NULL;
+  
+  last_node->nxt=new_node;
 
 
   
-
-void reverse(node* head){
-   
-//reverse it!
-
-  }}
-
-
-int main(){
-
-
-  node* head=malloc(sizeof(node));
-  head->data=0;
-  head->nxt=NULL;
-
-for(int i=1;i<5;i++){
-
-  node* nd=malloc(sizeof(node));
-  nd->data=i;
-  nd->nxt=NULL;
-  add_node(&head,nd);
   }
 
-  print_ll(head);
- 
+node* get_nthnode(node* head,int n){
+  node* tmp=head;
+  while(n){
+    tmp=tmp->nxt;
+  n--;
+  }
+  return tmp;
 }
+
+
+
+node* reverse(node* head){
+  node* tmp=head;
+  node* current=head;
+  node* prev=NULL;
+  node* next=NULL;
+
+
+  while(current){
+  
+    next=current->nxt;
+    current->nxt=prev;
+    prev=current;
+    current=next;
+
+
+  //rv_logic
+
+
+
+
+  }
+  return prev;
+
+}
+
+int main(){
+  node* head=malloc(sizeof(node));
+  head->data=1;
+
+  int arr[]={2,4,5,6,-1}; 
+
+  int i=0;
+  while(arr[i]!=-1){
+
+
+    append_node(head,arr[i]);
+    i++;
+  }
+
+  node* rv_head=reverse(head);
+  node* tmp=rv_head;
+  while(tmp){
+    printf("%d\n",tmp->data);
+  tmp=tmp->nxt;
+  }
+
+
+    //node* nth_node=get_nthnode(head,4);
+   // printf("%d\n",nth_node->data);
+  return 0;
+}
+
+
 
 
 
